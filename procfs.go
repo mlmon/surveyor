867823fb817d0884576e0cd3f11cb696da72eb5e
Nathan Fisher <nfisher@junctionbox.ca>
2025-01-29 22:19:48 -0500

    b2s breaks build on linux use slice instead of fixed array

--- procfs.go
+++ procfs.go
@@ -6,45 +6,47 @@ import (
 	"strings"
 )
 
-func ProcFS(procfs string) (*SourceRecords, error) {
-	var records []Record
+func ProcFS(procfs string) SourceFn {
+	return func() (*SourceRecords, error) {
+		var records []Record
 
-	err := filepath.Walk(procfs, func(path string, info os.FileInfo, err error) error {
+		err := filepath.Walk(procfs, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				// If we have a permissions error or similar, just skip this entry
+				return nil
+			}
+
+			// skip directories
+			if info.IsDir() {
+				return nil
+			}
+
+			// Read the file
+			data, err := os.ReadFile(path)
+			if err != nil {
+				// Could not read file (permission error, etc.), skip
+				return nil
+			}
+			value := strings.TrimSpace(string(data))
+
+			// Convert the path `/proc/sys/net/ipv4/ip_forward` to `net.ipv4.ip_forward`
+			// 1. Strip off "/proc/sys/"
+			relative := strings.TrimPrefix(path, procfs+"/")
+			// 2. Replace "/" with "."
+			paramName := strings.ReplaceAll(relative, "/", ".")
+
+			records = append(records, Record{Key: paramName, Value: value})
+
+			return nil
+		})
+
 		if err != nil {
-			// If we have a permissions error or similar, just skip this entry
-			return nil
+			return nil, err
 		}
 
-		// skip directories
-		if info.IsDir() {
-			return nil
-		}
-
-		// Read the file
-		data, err := os.ReadFile(path)
-		if err != nil {
-			// Could not read file (permission error, etc.), skip
-			return nil
-		}
-		value := strings.TrimSpace(string(data))
-
-		// Convert the path `/proc/sys/net/ipv4/ip_forward` to `net.ipv4.ip_forward`
-		// 1. Strip off "/proc/sys/"
-		relative := strings.TrimPrefix(path, procfs+"/")
-		// 2. Replace "/" with "."
-		paramName := strings.ReplaceAll(relative, "/", ".")
-
-		records = append(records, Record{Key: paramName, Value: value})
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
+		return &SourceRecords{
+			Source:  "procfs",
+			Records: records,
+		}, nil
 	}
-
-	return &SourceRecords{
-		Source:  "procfs",
-		Records: records,
-	}, nil
 }
--- uname.go
+++ uname.go
@@ -13,11 +13,11 @@ func Uname() (*SourceRecords, error) {
 	}
 	var records []Record
 
-	records = append(records, Record{"machine", b2s(uname.Machine)})
-	records = append(records, Record{"nodename", b2s(uname.Nodename)})
-	records = append(records, Record{"release", b2s(uname.Release)})
-	records = append(records, Record{"sysname", b2s(uname.Sysname)})
-	records = append(records, Record{"version", b2s(uname.Version)})
+	records = append(records, Record{"machine", b2s(uname.Machine[:])})
+	records = append(records, Record{"nodename", b2s(uname.Nodename[:])})
+	records = append(records, Record{"release", b2s(uname.Release[:])})
+	records = append(records, Record{"sysname", b2s(uname.Sysname[:])})
+	records = append(records, Record{"version", b2s(uname.Version[:])})
 	return &SourceRecords{
 		Source:  "uname",
 		Records: records,
@@ -25,7 +25,7 @@ func Uname() (*SourceRecords, error) {
 }
 
 // b2s converts a null terminated byte array to a string.
-func b2s(b [256]byte) string {
-	n := bytes.IndexByte(b[:], 0)
+func b2s(b []byte) string {
+	n := bytes.IndexByte(b, 0)
 	return string(b[:n])
 }
